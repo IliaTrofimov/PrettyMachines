00b1bb34f482cbe502e90ce8c1c7345e3bc8925b
Ilia Trofimov <ilia.trofimov2001@icloud.com>
2026-06-09 23:52:33 +0300

    Moved MarkovAlgorithmParser

---
+++ PrettyMachines.Algorithms/Utils/Parsing/MarkovSubstitutionParser.cs
@@ -0,0 +1,20 @@
+using PrettyMachines.Algorithms.Markov;
+
+
+namespace PrettyMachines.Algorithms.Utils.Parsing;
+
+/// <summary>
+/// Text parser that creates <see cref="Substitution"/> objects.
+/// </summary>
+public sealed class MarkovSubstitutionParser
+{
+    public Substitution ParseQuoted(string text, char quote = '\'')
+    {
+        throw new NotImplementedException();
+    }
+
+    public Substitution ParseUnquoted(string text)
+    {
+        throw new NotImplementedException();
+    }
+}
